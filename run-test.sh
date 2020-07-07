e0cfa99167f7680cd41019f9b3ca774a416b7174
shyam <shyamsundar.shanmugam@wipro.com>
2020-07-07 12:22:04 +0530

    waiting for completion

--- run-test.sh
+++ run-test.sh
@@ -6,9 +6,18 @@ for i in 1 2 3; do
   kubectl create -f job-0$i.yml
 done
 
-printf "Waiting for 5 seconds..."
-sleep 5
+printf "Waiting for job to complete"
+
+expected='1 1 1'
+actual=''
+while [ "$expected" != "$actual" ]; do
+  actual=$(kubectl get jobs -o jsonpath={.items[*].status.succeeded})
+  sleep 1
+  printf '.'
+done
 echo done
+
+#kubectl get pods -l app=env-test -o jsonpath={.items[*].status.containerStatuses[*]}
 
 for i in 1 2 3; do
   pod=$(kubectl get pod -l job-name=job-0$i -o jsonpath={.items[*].metadata.name})
